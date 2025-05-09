5392af5f49686e872ae3daa09fddb29e1c10789a
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-05-09 11:03:24 +0300

    __init__.py

---
+++ src/database/models/__init__.py
@@ -0,0 +1,17 @@
+from database.models.accounts import (
+    ActivationTokenModel,
+    UserGroupModel,
+    UserModel,
+    UserPurchasedMoviesModel,
+)
+from database.models.movies import (
+    CertificationModel,
+    CommentModel,
+    DirectorModel,
+    GenreModel,
+    MovieModel,
+    StarModel,
+)
+from database.models.orders import OrderItemModel, OrderModel
+from database.models.payment import PaymentItemModel, PaymentModel
+from database.models.shopping_cart import CartItemModel, CartModel
